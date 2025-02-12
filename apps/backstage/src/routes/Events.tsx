c054286b4c94a97ebff5f9538d927f1eea3dddc0
Arnold Gandarillas Castillo <arkgast@gmail.com>
2025-02-12 10:17:49 -0400

    feat(backstage): enable events feature in dashboard

--- apps/backstage/src/routes/Events.tsx
+++ apps/backstage/src/routes/Events.tsx
@@ -1,3 +1,31 @@
+import { Spinner } from '@luminova/ui';
+import { AddEventDialog, EventTable, useEvents } from '../features/events';
+
 export default function Events() {
-  return <div>Events</div>;
+  const { data: events = [], isLoading, isError, error } = useEvents();
+
+  if (isLoading && events.length === 0) {
+    return (
+      <div className="flex w-full justify-center border border-gray-100 p-5">
+        <Spinner className="h-8 w-8" />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="bg-red-50">Error fetching members: {error?.message}</div>
+    );
+  }
+
+  return (
+    <div>
+      <div className="mb-4 flex items-center justify-between">
+        <h1 className="text-3xl font-bold">Events Management</h1>
+        <AddEventDialog />
+      </div>
+
+      <EventTable events={events} />
+    </div>
+  );
 }
