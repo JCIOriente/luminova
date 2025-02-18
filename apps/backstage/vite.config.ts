1479bf18c0707843b8d0d7b2d1761c2bc90d3618
Arnold Gandarillas Castillo <arkgast@gmail.com>
2025-02-18 17:57:00 -0400

    feat(backstage): use env vars properly

--- apps/backstage/vite.config.ts
+++ apps/backstage/vite.config.ts
@@ -1,42 +1,64 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
-export default defineConfig({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/backstage',
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-  build: {
-    outDir: '../../dist/apps/backstage',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the mode (like 'development', 'production')
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    root: __dirname,
+    cacheDir: '../../node_modules/.vite/apps/backstage',
+    server: {
+      port: 4200,
+      host: 'localhost',
     },
-  },
-  test: {
-    watch: false,
-    globals: true,
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: '../../coverage/apps/backstage',
-      provider: 'v8',
+    preview: {
+      port: 4300,
+      host: 'localhost',
     },
-  },
+    plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+    build: {
+      outDir: '../../dist/apps/backstage',
+      emptyOutDir: true,
+      reportCompressedSize: true,
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+    },
+    define: {
+      'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV),
+      'process.env.FIREBASE_API_KEY': JSON.stringify(env.VITE_FIREBASE_API_KEY),
+      'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(
+        env.VITE_FIREBASE_AUTH_DOMAIN,
+      ),
+      'process.env.FIREBASE_PROJECT_ID': JSON.stringify(
+        env.VITE_FIREBASE_PROJECT_ID,
+      ),
+      'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(
+        env.VITE_FIREBASE_STORAGE_BUCKET,
+      ),
+      'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(
+        env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+      ),
+      'process.env.FIREBASE_APP_ID': JSON.stringify(env.VITE_FIREBASE_APP_ID),
+    },
+    test: {
+      watch: false,
+      globals: true,
+      environment: 'jsdom',
+      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      reporters: ['default'],
+      coverage: {
+        reportsDirectory: '../../coverage/apps/backstage',
+        provider: 'v8',
+      },
+    },
+  };
 });
