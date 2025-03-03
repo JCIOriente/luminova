88d4da14780e599f78151a20f9af93ee66b16931
Arnold Gandarillas Castillo <arkgast@gmail.com>
2025-03-03 12:00:55 -0400

    feat(beacon): add awardPoints firebase function

--- apps/beacon/src/main.ts
+++ apps/beacon/src/main.ts
@@ -1,1 +1,37 @@
-console.log('Hello World');
+import { initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import { onDocumentWritten } from 'firebase-functions/v2/firestore';
+
+initializeApp();
+
+const db = getFirestore();
+
+exports.awardPoints = onDocumentWritten('/events/{id}', async (event) => {
+  const date = new Date();
+  const memberPointsRef = db
+    .collection('memberPoints')
+    .doc(date.getFullYear() + '')
+    .collection(date.getMonth() + 1 + '')
+    .doc(event.params.id);
+
+  const afterData = event.data.after.data() || {};
+  const currentCoDirectorIds = afterData.coDirectorIds || [];
+  const currentCollaboratorIds = afterData.collaboratorIds || [];
+  const currentAssistantIds = afterData.assistantIds || [];
+
+  const memberPointsSnapshot = await memberPointsRef.get();
+  const dbOperation = memberPointsSnapshot.exists ? 'update' : 'set';
+  const resDoc = await memberPointsRef[dbOperation]({
+    director: afterData.directorId,
+    name: afterData.name,
+    coDirectorIds: currentCoDirectorIds,
+    collaboratorIds: currentCollaboratorIds,
+    assistantIds: currentAssistantIds,
+  });
+
+  console.log(
+    "Document operation '%s' executed at time: %s",
+    dbOperation,
+    resDoc.writeTime.toDate(),
+  );
+});
