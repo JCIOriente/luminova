fa321b7aa7ac18e80c9e491368999496802fdbed
Arnold Gandarillas Castillo <arkgast@gmail.com>
2026-06-06 11:31:05 -0400

    feat(types): wire engine barrel + ./engine pure subpath export

---
+++ packages/types/src/engine/index.ts
@@ -0,0 +1,34 @@
+export type { InitiativeRoster, FinalReport, InitiativeStatus } from "./initiative";
+export { INITIATIVE_STATUSES } from "./initiative";
+export type { Term, BoardSeat, TermStatus } from "./term";
+export { TERM_STATUSES } from "./term";
+export type { Program } from "./program";
+export type { Project } from "./project";
+export type {
+  Activity,
+  ActivityCategory,
+  ActivityOrganizers,
+  ActivityStatus,
+  InitiativeKind,
+} from "./activity";
+export { ACTIVITY_CATEGORIES, ACTIVITY_STATUSES } from "./activity";
+export type { PointRule, PointRuleCode } from "./point-rule";
+export { POINT_RULE_CODES, DEFAULT_POINT_VALUES } from "./point-rule";
+export type {
+  Participation,
+  ParticipationRole,
+  ParticipationState,
+  ParticipationGates,
+} from "./participation";
+export { PARTICIPATION_ROLES, PARTICIPATION_STATES } from "./participation";
+export type { MemberPoints } from "./member-points";
+export { resolvePointRuleCode, type ResolvePointRuleInput } from "./resolve-point-rule";
+export { computePunctualityFactor, type ComputePunctualityInput } from "./compute-punctuality";
+export {
+  isExecutiveCommittee,
+  wonBestMemberPreviousTerm,
+  evaluateEligibility,
+  type IneligibilityReason,
+  type EvaluateEligibilityInput,
+  type EligibilityResult,
+} from "./eligibility";
--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -3,3 +3,7 @@ export { MEMBER_STATUSES } from "./member";
 export { memberSchema, type MemberInput } from "./member-schema";
 export type { Ally } from "./ally";
 export { allySchema, type AllyInput } from "./ally-schema";
+
+export * from "./engine";
+export { pointRuleSchema, type PointRuleInput } from "./engine/point-rule-schema";
+export { activitySchema, type ActivityInput } from "./engine/activity-schema";
